0142961525b7b75f4258354dc55c6f1c472f6239
iqbal1361 <iqbalhossmd814@gmail.com>
2021-02-09 16:02:26 +0600

    Laravel Service Provider

---
+++ app/Providers/TestServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        app()->bind('Hello', function(){
+            return "Hii";
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
